182eecbe641f6494a081729606f066a0e045c940
bsWiwat <bs.wiwat@gmail.com>
2024-11-07 19:56:52 +0700

    add(): game interface.

--- src/utils/GameInterface.java
+++ src/utils/GameInterface.java
@@ -1,4 +1,24 @@
 package utils;
 
+import java.awt.Graphics;
+
 public interface GameInterface {
+
+    // Methods for character actions
+    void jump();
+    void shootFireball();
+    void changeX(int value);
+
+    // Methods for updating game elements
+    void updatePosition();
+    void updateAnimation();
+    void updateFireballs();
+
+    // Method for drawing the health bar
+    void drawHealthBar(Graphics g);
+    void drawScore (Graphics g);
+
+    // Method for applying effects
+    void applySlowdown();
+    void applyDamage();
 }
